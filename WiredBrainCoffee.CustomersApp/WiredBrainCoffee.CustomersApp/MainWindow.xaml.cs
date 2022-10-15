56ab0fac9ecdd83eec57cd49edb839ac590c08ab
jjokela <jarmo.jokela@gmail.com>
2022-10-15 22:36:26 +0200

    initial commit

---
+++ WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.ViewModel;
+
+namespace WiredBrainCoffee.CustomersApp
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainViewModel ViewModel { get; }
+        
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            Title = "Customers App";
+            ViewModel = new MainViewModel(new CustomerDataProvider());
+            root.DataContext = ViewModel;
+            root.Loaded += Root_Loaded;
+        }
+
+        private async void Root_Loaded(object sender, RoutedEventArgs e)
+        {
+            await ViewModel.LoadAsync();
+        }
+
+        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            // var column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
+            var column = Grid.GetColumn(customerListGrid);
+
+            var newColumn = column == 0 ? 2 : 0;
+
+            // customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+            Grid.SetColumn(customerListGrid, newColumn);
+
+            symbolIconMoveNavigation.Symbol = newColumn == 0 ? Symbol.Forward : Symbol.Back;
+        }
+    }
+}
---
+++ WiredBrainCoffee.CustomersApp/WiredBrainCoffee.CustomersApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.Model;
+
+namespace WiredBrainCoffee.CustomersApp.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly ICustomerDataProvider _customerDataProvider;
+
+        public ObservableCollection<Customer> Customers { get; } = new();
+
+        private Customer? _selectedCustomer;
+        public Customer? SelectedCustomer
+        {
+            get => _selectedCustomer;
+            set
+            {
+                if (_selectedCustomer != value)
+                {
+                    _selectedCustomer = value;
+                    RaisePropertyChanged();
+                }
+
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public MainViewModel(ICustomerDataProvider customerDataProvider)
+        {
+            _customerDataProvider = customerDataProvider;
+        }
+
+        public async Task LoadAsync()
+        {
+            if (Customers.Any())
+            {
+                return;
+            }
+
+            var customers = await _customerDataProvider.GetAllAsync();
+            if (customers is not null)
+            {
+                foreach (var customer in customers)
+                {
+                    Customers.Add(customer);
+                }
+            }
+        }
+
+        private void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
